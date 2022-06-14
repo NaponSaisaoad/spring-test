0e0e40b95dfb67e8277ff40e90279f57a6030c6b
Napon Saisaoad <napon@odds.team>
2022-06-14 22:12:07 +0700

    Add unit test save employee

--- src/test/java/net/javaguides/springboottesting/repository/EmployeeRepositoryTests.java
+++ src/test/java/net/javaguides/springboottesting/repository/EmployeeRepositoryTests.java
@@ -1,4 +1,31 @@
 package net.javaguides.springboottesting.repository;
 
+import net.javaguides.springboottesting.model.Employee;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
 public class EmployeeRepositoryTests {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @DisplayName("JUnit test for save employee")
+    @Test
+    public void givenEmployeeObject_whenSave_thenReturnSavedEmployee() {
+
+        Employee employee = Employee.builder()
+                .firstName("Napon")
+                .lastName("Saisaoad")
+                .email("NaponSaisaoad@gmail.com")
+                .build();
+
+        Employee employeeSaved = employeeRepository.save(employee);
+
+        assertThat(employeeSaved).isNotNull();
+        assertThat(employeeSaved.getId()).isGreaterThan(0);
+    }
 }
