131cac89ccbca754d9c57add29a0624e3b298fcf
Napon Saisaoad <napon@odds.team>
2022-06-15 21:05:34 +0700

    Add test for get All employee

--- src/test/java/net/javaguides/springboottesting/repository/EmployeeRepositoryTests.java
+++ src/test/java/net/javaguides/springboottesting/repository/EmployeeRepositoryTests.java
@@ -7,13 +7,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.List;
+
 @DataJpaTest
 public class EmployeeRepositoryTests {
 
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @DisplayName("JUnit test for save employee")
+    @DisplayName("JUnit test for save employee operation")
     @Test
     public void givenEmployeeObject_whenSave_thenReturnSavedEmployee() {
 
@@ -27,5 +29,53 @@ public class EmployeeRepositoryTests {
 
         assertThat(employeeSaved).isNotNull();
         assertThat(employeeSaved.getId()).isGreaterThan(0);
+    }
+
+    @DisplayName("JUnit test for get All employee operation")
+    @Test
+    public void giveEmployeeList_whenFindAll_thenEmployeeList() {
+
+        Employee employee_1 = Employee.builder()
+                .firstName("Napon")
+                .lastName("Saisaoad")
+                .email("NaponSaisaoad@gmail.com")
+                .build();
+        Employee employee_2 = Employee.builder()
+                .firstName("Bunphot")
+                .lastName("Saisaoad")
+                .email("BunphotSaisaoad@gmail.com")
+                .build();
+
+        employeeRepository.save(employee_1);
+        employeeRepository.save(employee_2);
+
+        List<Employee> employeeList = employeeRepository.findAll();
+
+        assertThat(employeeList).isNotNull();
+        assertThat(employeeList.size()).isEqualTo(2);
+    }
+
+    @DisplayName("JUnit test for get All employee operation")
+    @Test
+    public void giveEmployeeList_whenGetId_thenEmployeeList() {
+
+        Employee employee_1 = Employee.builder()
+                .firstName("Napon")
+                .lastName("Saisaoad")
+                .email("NaponSaisaoad@gmail.com")
+                .build();
+        Employee employee_2 = Employee.builder()
+                .firstName("Bunphot")
+                .lastName("Saisaoad")
+                .email("BunphotSaisaoad@gmail.com")
+                .build();
+
+        employeeRepository.save(employee_1);
+        employeeRepository.save(employee_2);
+
+        List<Employee> employeeList = employeeRepository.findAll();
+
+        assertThat(employeeList).isNotNull();
+        assertThat(employeeList.size()).isEqualTo(2);
     }
 }
