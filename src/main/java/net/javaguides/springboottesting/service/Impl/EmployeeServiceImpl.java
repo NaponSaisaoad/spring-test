675326e340c2881c783a49d1f35d5eed46a371de
Napon Saisaoad <napon@odds.team>
2022-06-20 20:03:28 +0700

    Add employee service

---
+++ src/main/java/net/javaguides/springboottesting/service/Impl/EmployeeServiceImpl.java
@@ -0,0 +1,22 @@
+package net.javaguides.springboottesting.service.Impl;
+
+import net.javaguides.springboottesting.model.Employee;
+import net.javaguides.springboottesting.repository.EmployeeRepository;
+import net.javaguides.springboottesting.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+
+        return employeeRepository.save(employee);
+    }
+}
