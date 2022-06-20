10dd8b004273151b7f1780b2875e99585415c686
Napon Saisaoad <napon@odds.team>
2022-06-20 21:29:08 +0700

    Add test for for saveEmployee method which throws exception

--- src/test/java/net/javaguides/springboottesting/service/EmployeeServiceTests.java
+++ src/test/java/net/javaguides/springboottesting/service/EmployeeServiceTests.java
@@ -1,5 +1,6 @@
 package net.javaguides.springboottesting.service;
 
+import net.javaguides.springboottesting.exception.ResourceNotFoundException;
 import net.javaguides.springboottesting.model.Employee;
 import net.javaguides.springboottesting.repository.EmployeeRepository;
 import net.javaguides.springboottesting.service.Impl.EmployeeServiceImpl;
@@ -9,7 +10,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -49,5 +55,18 @@ public class EmployeeServiceTests {
         Employee saveEmployee = employeeService.saveEmployee(employee);
 
         Assertions.assertThat(saveEmployee).isNotNull();
+    }
+
+    @DisplayName("JUnit test for for saveEmployee method which throws exception")
+    @Test
+    public void givenExistingEmail_whenSaveEmployee_thenThrowsException() {
+
+        given(employeeRepository.findByEmail(employee.getEmail())).willReturn(Optional.of(employee));
+
+        org.junit.jupiter.api.Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            employeeService.saveEmployee(employee);
+        });
+
+        verify(employeeRepository, never()).save(any(Employee.class));
     }
 }
