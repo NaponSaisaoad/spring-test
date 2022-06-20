63f300f4b8f3ac830a31d19f1abb295342abb553
Napon Saisaoad <napon@odds.team>
2022-06-20 20:18:58 +0700

    Add setup employee

--- src/test/java/net/javaguides/springboottesting/service/EmployeeServiceTests.java
+++ src/test/java/net/javaguides/springboottesting/service/EmployeeServiceTests.java
@@ -1,4 +1,19 @@
 package net.javaguides.springboottesting.service;
 
+import net.javaguides.springboottesting.repository.EmployeeRepository;
+import net.javaguides.springboottesting.service.Impl.EmployeeServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
+
 public class EmployeeServiceTests {
+
+    private EmployeeRepository employeeRepository;
+
+    private EmployeeService employeeService;
+
+    @BeforeEach
+    public void setup(){
+        employeeRepository = Mockito.mock(EmployeeRepository.class);
+        employeeService = new EmployeeServiceImpl(employeeRepository);
+    }
 }
