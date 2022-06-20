33d9d8277a9e8ad2878a47b2e29fb4d93462aaf1
Napon Saisaoad <napon@odds.team>
2022-06-20 20:52:00 +0700

    Add test for for saveEmployee method

--- src/test/java/net/javaguides/springboottesting/service/EmployeeServiceTests.java
+++ src/test/java/net/javaguides/springboottesting/service/EmployeeServiceTests.java
@@ -1,9 +1,16 @@
 package net.javaguides.springboottesting.service;
 
+import net.javaguides.springboottesting.model.Employee;
 import net.javaguides.springboottesting.repository.EmployeeRepository;
 import net.javaguides.springboottesting.service.Impl.EmployeeServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+
+import java.util.Optional;
 
 public class EmployeeServiceTests {
 
@@ -15,5 +22,23 @@ public class EmployeeServiceTests {
     public void setup(){
         employeeRepository = Mockito.mock(EmployeeRepository.class);
         employeeService = new EmployeeServiceImpl(employeeRepository);
+    }
+
+
+    @DisplayName("JUnit test for for saveEmployee method")
+    @Test
+    public void givenEmployeeObject_whenSaveEmployee_thenReturnEmployeeObject() {
+      Employee employee = Employee.builder()
+                .firstName("Napon")
+                .lastName("Saisaoad")
+                .email("NaponSaisaoad@gmail.com")
+                .build();
+        BDDMockito.given(employeeRepository.findByEmail(employee.getEmail())).willReturn(Optional.empty());
+
+        BDDMockito.given(employeeRepository.save(employee)).willReturn(employee);
+
+        Employee saveEmployee = employeeService.saveEmployee(employee);
+
+        Assertions.assertThat(saveEmployee).isNotNull();
     }
 }
