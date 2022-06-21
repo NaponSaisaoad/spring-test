3405877d1cfd8c611839aa14f259b1cc99988ed6
Napon Saisaoad <napon@odds.team>
2022-06-21 21:34:17 +0700

    Add EmployeeController

---
+++ src/main/java/net/javaguides/springboottesting/controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package net.javaguides.springboottesting.controller;
+
+import net.javaguides.springboottesting.service.EmployeeService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+}
