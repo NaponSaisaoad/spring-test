70bbfdc05604af49372aa5cfd02c8d9585da6b1d
Napon Saisaoad <napon@odds.team>
2022-06-21 21:40:51 +0700

    Add createEmployee

--- src/main/java/net/javaguides/springboottesting/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboottesting/controller/EmployeeController.java
@@ -1,8 +1,9 @@
 package net.javaguides.springboottesting.controller;
 
+import net.javaguides.springboottesting.model.Employee;
 import net.javaguides.springboottesting.service.EmployeeService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -12,5 +13,11 @@ public class EmployeeController {
 
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
     }
 }
